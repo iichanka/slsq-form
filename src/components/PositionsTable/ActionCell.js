325519cb88412f94db9c0ff59a26d9ee88506fc0
iichanka <iichanka@mail.ru>
2018-09-06 12:12:51 +0500

    Autodelivery

--- src/components/PositionsTable/ActionCell.js
+++ src/components/PositionsTable/ActionCell.js
@@ -1,31 +1,45 @@
 import React                            from 'react';
+import PropTypes                        from 'prop-types';
 import { Button }                       from 'antd';
 import { store }                        from '../../index';
 import { deletePositions }              from '../../actions/positions/deletePositions'
 
-export const ActionCell = ({ data, editMode }) => {
-  return(
-    <div>
-      {
-        data.actions.delete && editMode &&
-        <Button
-          size      = 'small' 
-          icon      = 'delete'
-          className = 'field-no-wrap'
-          onClick   = { (e) => { store.dispatch(deletePositions([data])) } }
-        /> 
-      }
-      {
-        data.actions.autodelivery &&
-        <Button
-          size      = 'small'
-          icon      = 'car'
-          className = 'field-no-wrap'
-          onClick   = { (e) => { alert('В разработке'); } }
-        />
-      }
-    </div>
-  );   
+export class ActionCell extends React.Component {
+  static propTypes = { 
+    editMode:         PropTypes.bool.isRequired,    // режим редактирования?
+    data:             PropTypes.object.isRequired,  //record таблицы позиций
+  }
+
+
+  onAutodeliveryClick = (event) => {
+    //getAutodeliveryDetails
+    //showPopup
+  }
+  
+  render = () => {
+    return(
+      <div>
+        {
+          this.props.data.actions.delete && this.props.editMode &&
+          <Button
+            size      = 'small' 
+            icon      = 'delete'
+            className = 'field-no-wrap'
+            onClick   = { (e) => { store.dispatch(deletePositions([data])) } }
+          /> 
+        }
+        {
+          this.props.data.actions.autodelivery &&
+          <Button
+            size      = 'small'
+            icon      = 'car'
+            className = 'field-no-wrap'
+            onClick   = { this.onAutodeliveryClick }
+          />
+        }
+      </div>
+    );   
+  } 
 }
 
 export default ActionCell;
